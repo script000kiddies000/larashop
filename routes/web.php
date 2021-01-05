37fa32f53415b747edfb4e6615c6f821e6a90c38
script000kiddies000 <scriptkiddies@protonmail.com>
2021-01-05 21:00:33 +0700

    manage categories

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,8 @@ Route::match(["GET", "POST"], "/register", function(){
 });
 
 Route::resource("users", UserController::class);
+
+Route::get('/categories/trash', [CategoryController::class, 'trash'])->name('categories.trash');
+Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+Route::delete('/categories/{category}/delete-permanent', [CategoryController::class, 'deletePermanent'])->name('categories.delete-permanent');
+Route::resource('categories', CategoryController::class);
